e2d236955a8113477b404a0b04030193e09a307c
pranit <pranitsakunde@gmal.com>
2019-11-14 11:54:38 +0530

    myFirstCommit

---
+++ src/main/java/com/app/ExpenseManagerLast/ExpenseManagerlastApplication.java
@@ -0,0 +1,12 @@
+package com.app.ExpenseManagerLast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerlastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagerlastApplication.class, args);
+	}
+}
